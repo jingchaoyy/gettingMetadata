fb9f80e151adf8e292d5427a1b0d5d8be503f5cd
jingchaoyy <jcyangt@gmail.com>
2018-04-30 13:26:44 -0400

    starting with tiff

---
+++ tiff/tiffMeta.py
@@ -0,0 +1,69 @@
+"""
+Created on 4/30/18
+
+@author: YJccccc
+"""
+
+from osgeo import gdal,ogr,osr
+
+
+def GetExtent(gt,cols,rows):
+    ''' Return list of corner coordinates from a geotransform
+
+        @type gt:   C{tuple/list}
+        @param gt: geotransform
+        @type cols:   C{int}
+        @param cols: number of columns in the dataset
+        @type rows:   C{int}
+        @param rows: number of rows in the dataset
+        @rtype:    C{[float,...,float]}
+        @return:   coordinates of each corner
+    '''
+    ext=[]
+    xarr=[0,cols]
+    yarr=[0,rows]
+
+    for px in xarr:
+        for py in yarr:
+            x=gt[0]+(px*gt[1])+(py*gt[2])
+            y=gt[3]+(px*gt[4])+(py*gt[5])
+            ext.append([x,y])
+            # print (x,y)
+        yarr.reverse()
+    return ext
+
+def ReprojectCoords(coords,src_srs,tgt_srs):
+    ''' Reproject a list of x,y coordinates.
+
+        @type geom:     C{tuple/list}
+        @param geom:    List of [[x,y],...[x,y]] coordinates
+        @type src_srs:  C{osr.SpatialReference}
+        @param src_srs: OSR SpatialReference object
+        @type tgt_srs:  C{osr.SpatialReference}
+        @param tgt_srs: OSR SpatialReference object
+        @rtype:         C{tuple/list}
+        @return:        List of transformed [[x,y],...[x,y]] coordinates
+    '''
+    trans_coords=[]
+    transform = osr.CoordinateTransformation( src_srs, tgt_srs)
+    for x,y in coords:
+        x,y,z = transform.TransformPoint(x,y)
+        trans_coords.append([x,y])
+    return trans_coords
+
+
+
+filepath = r"/Users/YJccccc/Desktop/DonglianSun_Project/SNPP_VIIRS_375m_floodmap_1826_Sep01_2017_Geographic_geotiff/SNPP_VIIRS_375m_floodmap_1826_Sep01_2017_Geographic.tif"
+
+# Open the file:
+raster = gdal.Open(filepath)
+
+meta = raster.GetMetadata()
+
+gt=raster.GetGeoTransform()
+cols = raster.RasterXSize
+rows = raster.RasterYSize
+ext=GetExtent(gt,cols,rows)
+print('Extent:', ext)
+
+
